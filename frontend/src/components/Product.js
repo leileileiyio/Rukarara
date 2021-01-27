74f31d324d3fd74d1aa7bc1dafa2d9089b38e25d
Lei Xuan <lei.xuan@duke.edu>
2021-01-26 23:22:09 -0500

    add product screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,13 +6,13 @@ export default function Product(props) {
     return (
         <div key={product.id} className="card">
 
-            <a href={'/product/${product._id}'}>
+            <a href={`/product/${product._id}`}>
                 <img className="medium" src={product.image} alt={product.name} />
             </a>
 
             <div className="card-body">
 
-                <a href={'/product/${product._id}'}><h2>{product.name}</h2></a>
+                <a href={`/product/${product._id}`}><h2>{product.name}</h2></a>
 
                 <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
 
