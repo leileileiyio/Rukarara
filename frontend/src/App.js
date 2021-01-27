0392ebf74039605950ffa4e7e618747d5094005a
Lei Xuan <lei.xuan@duke.edu>
2021-01-26 19:47:13 -0500

    add files to github

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Product from './components/Product';
+import data from './data';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">Rukarara</a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+
+      <main>
+        <div className="row center">
+          {
+            data.products.map(product => (<Product key={product._id} product={product}></Product>))
+          }
+
+        </div>
+
+      </main>
+
+      <footer className="row center">
+        All rights reserved
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
