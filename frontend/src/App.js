738479a97ced1107331da4b4177834502482284c
Lei Xuan <lei.xuan@duke.edu>
2021-01-29 19:15:53 -0500

    create register screen(bug: create a new account and sign out, then you can't create another account)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,18 @@ import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
   const userSignin = useSelector((state) => state.userSignin);
-  const {userInfo} = userSignin;
+  const { userInfo } = userSignin;
   const dispatch = useDispatch();
   const signoutHandler = () => {
     dispatch(signout());
-  }
-
+  };
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -33,25 +33,29 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            {
-              userInfo ? (
-                <div className="dropdown">
-                  <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i></Link>
-                  <ul className="dropdown-content">
-                    <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
-                  </ul>
-                </div>
-              ) :
-              (
-                <Link to="/signin">Sign In</Link>
-              )
-            }
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
           </div>
         </header>
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
