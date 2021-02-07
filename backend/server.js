040cc961ad129aec2501cea908ece14e02b6231a
Lei Xuan <lei.xuan@duke.edu>
2021-02-07 14:57:53 -0500

    fix typo errors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/rukarara', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/Rukarara', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
