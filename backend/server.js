eac3732c5f8737f271e564346ea4588a2fd47754
Lei Xuan <lei.xuan@duke.edu>
2021-02-07 14:56:36 -0500

    fix typo errors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/Rukarara', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/rukarara', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -25,12 +25,12 @@ app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
-
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
-
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
